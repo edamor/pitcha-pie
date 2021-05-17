788c4fbed161dae646fc75d28de6dd5ad8df9bf9
ed louise amor <ed.amor@xlog.com>
2021-05-17 23:53:04 +0800

    include service worker

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './globalStyle/colors.css';
 import './globalStyle/fonts.css';
 import './globalStyle/reference.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -14,7 +14,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+serviceWorker.register();
